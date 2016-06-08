0d28c675a2558485f1937599aa0ec19878956c0b
NatalliaYarmak <Natallia.Yarmak@gmail.com>
2016-06-08 23:15:27 +0300

    fix mistake in formatted output

--- testcucumber/src/test/java/n/yarmak/testcucumber/CukesTestSteps.java
+++ testcucumber/src/test/java/n/yarmak/testcucumber/CukesTestSteps.java
@@ -5,6 +5,6 @@ import cucumber.api.java.en.Given;
 public class CukesTestSteps {
     @Given("I have (\\d+) cukes in my belly")
     public void I_have_cukes_in_my_belly(int cukes) {
-        System.out.format("Cukes: %n\n", cukes);
+        System.out.format("Cukes: %d%n", cukes);
     }
 }
